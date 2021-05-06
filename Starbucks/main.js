941c57a71d85d0d99a069719032d0e81a4929f02
madinajabrayil <56426429+madinajabrayil@users.noreply.github.com>
2021-05-06 17:46:43 +0400

    Add files via upload

---
+++ Starbucks/main.js
@@ -0,0 +1,8 @@
+function imgSlider(imgSource) {
+    document.querySelector('.starbucks-coffee').src = imgSource
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
